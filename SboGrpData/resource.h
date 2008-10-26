1a0a0606d09a8dfe8e54845e0438ec7786d36610
RXP10430 <RXP10430@868715c2-6dae-4c12-b071-75cd31844a78>
2008-10-26 06:27:16 +0000

    画像の更新とマップ画像３を追加
    git-svn-id: svn://localhost/trunk@34 868715c2-6dae-4c12-b071-75cd31844a78

--- SboGrpData/resource.h
+++ SboGrpData/resource.h
@@ -19,12 +19,13 @@
 #define IDB_PNG14                       219
 #define IDB_PNG12                       220
 #define IDB_PNG16                       222
+#define IDB_PNG17                       223
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        223
+#define _APS_NEXT_RESOURCE_VALUE        224
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
