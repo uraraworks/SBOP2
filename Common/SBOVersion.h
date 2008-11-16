b02d25d68ec8f8724eff186d61422fb445013cd7
RXP10430 <RXP10430@868715c2-6dae-4c12-b071-75cd31844a78>
2008-11-16 05:27:41 +0000

    バージョンを更新
    git-svn-id: svn://localhost/trunk@68 868715c2-6dae-4c12-b071-75cd31844a78

--- Common/SBOVersion.h
+++ Common/SBOVersion.h
@@ -12,8 +12,8 @@
 /* 定数定義																	 */
 /* ========================================================================= */
 
-#define VERTEXT					"Ver0.027.2"						/* バージョン */
-#define VERSIONVAL				(0x00002702)						/* バージョン番号 */
+#define VERTEXT					"Ver0.028"							/* バージョン */
+#define VERSIONVAL				(0x00002800)						/* バージョン番号 */
 #define DLLVER					"1"									/* DLLバージョン */
 #define GRPDATADLLVER			"4"									/* グラフィックデータDLLバージョン */
 
