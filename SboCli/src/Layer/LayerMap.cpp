c678e8a8f60ec300df3637a7d14205e1093c5af2
RXP10430 <RXP10430@868715c2-6dae-4c12-b071-75cd31844a78>
2008-11-25 11:25:50 +0000

    ・スクロール時に重ね合わせマップパーツがキャラに重ならないことがあったのを修正。
    git-svn-id: svn://localhost/trunk@104 868715c2-6dae-4c12-b071-75cd31844a78

--- SboCli/src/Layer/LayerMap.cpp
+++ SboCli/src/Layer/LayerMap.cpp
@@ -1220,15 +1220,7 @@ void CLayerMap::DrawPartsPile(PCImg32 pDst, int nDrawY/*-99*/)
 			if ((pInfoMapParts->m_dwPartsType & (BIT_PARTSHIT_PILE | BIT_PARTSHIT_PILEBACK)) == 0) {
 				bDraw = FALSE;
 			}
-			if (nDrawY != -99) {
-				if (pInfoMapParts->m_dwPartsType & BIT_PARTSHIT_DRAWLAST) {
-					bDraw = FALSE;
-				}
-			} else {
-				if ((pInfoMapParts->m_dwPartsType & BIT_PARTSHIT_DRAWLAST) == 0) {
-					bDraw = FALSE;
-				}
-			}
+
 			if (bDraw) {
 				m_pMgrDraw->DrawMapParts (
 						pDst,
