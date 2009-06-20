d4986a9863fc04bc43d41350c5fad543ff45bfb0
RXP10430 <RXP10430@868715c2-6dae-4c12-b071-75cd31844a78>
2009-06-20 15:06:07 +0000

    ランダム移動しながら索敵するように対応中。
    git-svn-id: svn://localhost/trunk@241 868715c2-6dae-4c12-b071-75cd31844a78

--- SboSvr/src/Info/InfoChar/InfoCharBATTLE2Svr.cpp
+++ SboSvr/src/Info/InfoChar/InfoCharBATTLE2Svr.cpp
@@ -42,6 +42,57 @@ CInfoCharBATTLE2Svr::~CInfoCharBATTLE2Svr()
 
 BOOL CInfoCharBATTLE2Svr::TimerProcSTAND(DWORD dwTime)
 {
+	int nDirection;
+	DWORD dwTmp;
+	BOOL bRet, bResult;
+	POINT ptFront;
+
+	bRet = FALSE;
+
+	dwTmp = dwTime - m_dwLastTimeMove;
+	/* 1ターンは100msにしておく */
+	if (dwTmp < 100) {
+		goto Exit;
+	}
+
+	m_dwLastTimeMove = dwTime;
+	if (genrand () % 100 < 100 - m_nMoveAverage) {
+		if (m_dwTargetCharID == 0) {
+			m_bChgTargetChar = TRUE;
+		} else {
+			m_nReserveChgMoveState = CHARMOVESTATE_BATTLE;
+			m_bChgPos = TRUE;
+		}
+		goto Exit;
+	}
+
+	if (m_pInfoMap == NULL) {
+		goto Exit;
+	}
+	bResult = IsEnableMove ();
+	if (bResult == FALSE) {
+		goto Exit;
+	}
+
+	nDirection = genrand () % 4;
+	bResult = IsMoveDirection (nDirection);
+
+	/* 進める？ */
+	if (bResult) {
+		m_nDirection = nDirection;
+		GetFrontPos (ptFront, m_nDirection, TRUE);
+		SetPos (ptFront.x, ptFront.y);
+
+		m_bChgPos = TRUE;
+		SetMoveState (CHARMOVESTATE_MOVE);
+	}
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+
+
+#if 0
 	DWORD dwTmp;
 	BOOL bRet, bResult;
 
@@ -76,6 +127,7 @@ BOOL CInfoCharBATTLE2Svr::TimerProcSTAND(DWORD dwTime)
 	bRet = TRUE;
 Exit:
 	return bRet;
+#endif
 }
 
 /* Copyright(C)URARA-works 2009 */
