8d1bcec09b74f62ff24faa6470e616c74e7be216
Masakazu Yonehara <yonehara@urara-works.jp>
2026-07-18 13:44:10 +0900

    公式プレイガイドとサーバー状況表示を追加

--- SboSvr/src/Web/Handlers/HealthHandler.h
+++ SboSvr/src/Web/Handlers/HealthHandler.h
@@ -2,8 +2,14 @@
 
 #include "Web/ApiHandler.h"
 
+class CMgrData;
+
 class CHealthHandler : public IApiHandler
 {
 public:
+        explicit CHealthHandler(CMgrData *pMgrData);
         virtual void Handle(const HttpRequest &request, HttpResponse &response);
+
+private:
+        CMgrData *m_pMgrData;
 };
